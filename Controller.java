3bda791f6cf550bebf15b60d5c848aa636f823aa
Meyirbek <meiirbek.ongaibay@narxoz.kz>
2022-04-29 03:20:21 +0600

    'demo'

---
+++ Controller.java
@@ -0,0 +1,19 @@
+package kz.narxoz.sabak.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+
+public class Controller {
+    @GetMapping("/hi")
+        public String hi () {
+            return "hi guys";
+        }
+    @GetMapping("/bye")
+    public String bye () {
+        return "уйне кайт";
+    }
+}
